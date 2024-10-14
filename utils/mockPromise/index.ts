9eb3452fbcde9afec2cbcb224a06b7a6c6577b37
SeolJaeHyeok <milkboy2564@naver.com>
2024-10-14 15:47:33 +0900

    feature: add mock api request utils

---
+++ utils/mockPromise/index.ts
@@ -0,0 +1,33 @@
+type ApiResponse<T> = {
+    success: boolean;
+    data: T;
+    error?: string;
+};
+
+const mockApiRequest = ({
+    url,
+    delayPerRequest,
+}: {
+    url: string | string[];
+    delayPerRequest: number;
+}) => {
+    return new Promise((resolve) => {
+        const requestSuccessProbablity = Math.floor(Math.random() * 100); // 0 ~ 99
+        setTimeout(() => {
+            // API 실패 확률 10%
+            if (requestSuccessProbablity < 10) {
+                resolve({
+                    success: true,
+                    data: `Success Api Request from ${url}`,
+                    error: null,
+                });
+            } else {
+                resolve({
+                    success: false,
+                    data: `Failed Api Request from ${url}`,
+                    error: null,
+                });
+            }
+        }, delayPerRequest);
+    });
+};
